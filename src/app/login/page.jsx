ab4737433eea518afca80a4279f15d334e950cc8
Mojassam-raza <mojassamraza7@gmail.com>
2025-11-30 10:17:24 +0530

    sdk

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -13,7 +13,7 @@ const LoginSchema = Yup.object().shape({
     .required('nhi hai'),
   password: Yup.string()
     .required('password khan hai')
-  });
+});
 
 const Login = () => {
   const loginForm = useFormik({
@@ -21,23 +21,23 @@ const Login = () => {
       name: '',
       email: '',
       password: '',
-       
+
     },
     onSubmit: (value, { resetForm }) => {
       console.log(value);
       setTimeout(() => {
-        resetForm();  
+        resetForm();
       }, 3000);
       // submit values to backend
 
       axios.post('http://localhost:5000/user/authenticate', value)
-      .then((result) => {
-        toast.success('login successfull')
-        localStorage.setItem('token', result.data.token);
-      }).catch((err) => {
-        console.log(err);
-        toast.error('Invalid Email or Passeord');
-      });
+        .then((result) => {
+          toast.success('login successfull')
+          localStorage.setItem('token', result.data.token);
+        }).catch((err) => {
+          console.log(err);
+          toast.error('Invalid Email or Passeord');
+        });
     },
     validationSchema: LoginSchema,
   });
@@ -47,21 +47,21 @@ const Login = () => {
       <div className="max-w-lg mx-auto bg-white border border-gray-200 rounded-xl shadow-2xs">
 
 
-<div className="p-4 sm:p-7">
+        <div className="p-4 sm:p-7">
           <div className="text-center">
             <h1 className="block text-2xl font-bold text-gray-800">Log In</h1>
 
 
-<div className="text-center">
-            <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
-            <p className="mb-5 mt-2 text-sm text-gray-600 dark:text-neutral-500">
-              Don't have an account yet?
-              <a className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500" href="../examples/html/signup.html">
-                Sign up here
-              </a>
-            </p>
-          </div>
-<button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
+              <p className="mb-5 mt-2 text-sm text-gray-600 dark:text-neutral-500">
+                Don't have an account yet?
+                <a className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500" href="../examples/html/signup.html">
+                  Sign up here
+                </a>
+              </p>
+            </div>
+            <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
               <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
                 <path d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z" fill="#4285F4" />
                 <path d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z" fill="#34A853" />
@@ -70,9 +70,9 @@ const Login = () => {
               </svg>
               Sign in with Google
             </button>
-  
 
-        
+
+
             {/* Form */}
             <form onSubmit={loginForm.handleSubmit}>
               <div className="grid gap-y-4">
@@ -95,7 +95,7 @@ const Login = () => {
                 {/* Password */}
                 <div>
                   <label htmlFor="password" className="block text-sm mb-2 text-left px-3 font-xl text-black">Password</label>
-                  <input  
+                  <input
                     type="password"
                     id="password"
                     onChange={loginForm.handleChange}
@@ -107,14 +107,14 @@ const Login = () => {
                   )}
 
                   <div className="flex items-center justify-between mt-3 px-3">
-    <label className="flex items-center space-x-2 text-sm text-gray-700">
-      <input type="checkbox" className="h-4 w-4 text-blue-600 border-gray-300 rounded" />
-      <span>Remember me</span>
-    </label>
-    <a href="#" className="text-sm text-blue-600 hover:underline">
-      Forgot password?
-    </a>
-  </div>
+                    <label className="flex items-center space-x-2 text-sm text-gray-700">
+                      <input type="checkbox" className="h-4 w-4 text-blue-600 border-gray-300 rounded" />
+                      <span>Remember me</span>
+                    </label>
+                    <a href="#" className="text-sm text-blue-600 hover:underline">
+                      Forgot password?
+                    </a>
+                  </div>
                 </div>
 
 
@@ -143,7 +143,7 @@ const Login = () => {
         <button className='global-btn'>login global button</button>
         <button className={classes.localBtn}>local button</button>
       </div> */}
-     
+
     </div>
   );
 };
